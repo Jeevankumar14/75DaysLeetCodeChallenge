dfaf113d24d8844440af632152c5b5fef13bd1af
Jeevan kumar chitimalla <jeevankumar14805@gmail.com>
2026-03-20 13:42:44 +0530

    Time: 1 ms (78.89%), Space: 46.8 MB (58.06%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] arr) {
+        int i=1;
+        if(arr.length==0)return 0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[j]!=arr[i-1]){
+                arr[i]=arr[j];
+                i++;
+                
+            }
+        }
+        return i;
+        
+    }
+}
