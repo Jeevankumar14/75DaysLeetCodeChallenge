6ed9eca159e7b803e925f4f0eafe4d76b8418f3a
Jeevan kumar chitimalla <jeevankumar14805@gmail.com>
2026-05-01 13:13:18 +0530

    Time: 7 ms (9.54%), Space: 46.7 MB (52.32%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,13 +11,13 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> visited_nodes = new HashSet<>();
+        HashSet<ListNode> hs = new HashSet<>();
         ListNode current_node = head;
         while (current_node != null) {
-            if (visited_nodes.contains(current_node)) {
+            if (hs.contains(current_node)) {
                 return true;
             }
-            visited_nodes.add(current_node);
+            hs.add(current_node);
             current_node = current_node.next;
         }
         return false;
